c91e6c59dd8d4a506045ed723eeae89199e4e108
YulianGilyazev <45824886+YulianGilyazev@users.noreply.github.com>
2019-04-29 22:19:49 +0300

    Update args.py

--- args.py
+++ args.py
@@ -1,3 +1,3 @@
-params = dict(dbname="rasp", user="yulian", password="12369", host="localhost")
+params = dict(dbname="test", user="user", password="12345", host="localhost")
 HOST = '127.0.0.1'
 PORT = '5000'
